a403a239b4011a6e7135b4cf11613bd50c319e07
Uygar Gökhan Koca <gokhanuygar03@gmail.com>
2022-12-22 11:19:38 +0300

    update

--- PROJE.c
+++ PROJE.c
@@ -3,7 +3,7 @@
 /*Öğrencilerin Adı-Soyadı/Numarası/Fakülteleri
 Sergen Ege Gençer/22181616026/Teknoloji
 Uygar Gökhan Koca/22181616007/Teknoloji
-Yusuf Ferhat Yılmaz/221816160032/*/
+Yusuf Ferhat Yılmaz/221816160032/Teknoloji*/
 
 
 void kullanicicevabi(){
@@ -173,14 +173,14 @@ int main(){
     char yanit;
     
     printf("Hangi super kahramansiniz testimize hos geldiniz.\n");
-    printf("Testimize baslamak icin sirasiyla R'yi, istediginiz zaman cikmak icin Q'yu giriniz:");
+    printf("Testimize baslamak icin R'yi, istediginiz zaman cikmak icin Q'yu giriniz:");
     
-    basla = getchar();
+    scanf(" %c", &basla);
 
-    while((basla != 'R') && (basla != 'r') && (basla != 'Q') && (basla != 'q') && (basla == 10)){
+    while((basla != 'R') && (basla != 'r') && (basla != 'Q') && (basla != 'q')){
 
         printf("Lutfen R ya da Q'yu giriniz:");
-        basla = getchar();
+        scanf(" %c", &basla);
         
     }
 
